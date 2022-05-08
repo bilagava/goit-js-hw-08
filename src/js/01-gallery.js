a5299309b3c6d1f86d6bb9d443c7f5bc1ddb6adb
Yuriy Brovko <ysb.ptc@gmail.com>
2022-05-08 20:37:37 +0300

    js\01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+const gallery = galleryItems.map(({original, preview, description})=> 
+    `<li  class="gallery__item">
+        <a class="gallery__link" 
+            href="${original}">
+            <img class="gallery__image"
+            src="${preview}"
+            alt="${description}"/>
+        </a>
+    </li>`).join("");
+
+const galleryContainer = document.querySelector(".gallery");
+
+galleryContainer.insertAdjacentHTML("afterbegin", gallery);
+console.log(galleryItems);
+
+let ligthBox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt", 
+    captionPosition: "bottom",
+    captionDelay: 250,
+    });
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,40 @@
+import Player from '@vimeo/player';
+
+// const player = new Player('handstick', {
+//     id: 19231868,
+//     width: 640
+// });
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+
+const iframeEl = document.querySelector("#vimeo-player");
+console.log(iframeEl);
+
+const player = new Player(iframeEl);
+
+// const onPlay = function(data) {
+//     duration: 09.32
+//     percent: 0.049
+//     seconds: 1.000
+// };
+
+// player.on('play', onPlay);
+
+// currentTime
+
+// player.setCurrentTime(30.456).then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
 
